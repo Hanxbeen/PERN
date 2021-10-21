1b5deacc66dbc0e2953dc378f87fdab944b455fb
Hanxbeen <hanxbeen@gmail.com>
2021-10-21 15:51:08 +0900

    feat : 'db 연결 후 get, post 테스트 실패'

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "hb",
   host: "localhost",
-  database: "perntodo",
+  database: "postgres",
   password: "1111",
   port: 5433,
 });
--- server/index.js
+++ server/index.js
@@ -10,15 +10,45 @@ app.use(express.json());
 //ROUTES
 
 //create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { description } = req.body;
+    // const { description } = "I NEED TO CLEAN MY CAR";
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 //get all todos
-
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 //get a todo
 
 //update a todo
 
 //delete a todo
 
+//CONNECTION TEST
+
 app.listen(5000, () => {
-  console.log("PORT 5000 서버 실행");
+  console.log("SERVER STARTED AT PORT 5000");
+});
+pool.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("# DATABASE CONNECTED #");
+  }
 });
